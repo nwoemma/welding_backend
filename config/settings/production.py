fe4fc46431a2ebde318c6265b73279dce9b113c6
unknown <nwosuemmanuel159@gmail.com>
2025-08-06 16:28:04 +0100

    latest commit

--- config/settings/production.py
+++ config/settings/production.py
@@ -38,5 +38,31 @@ AUTH_USER_MODEL = 'accounts.User'
 
 # Logging configuration remains the same
 LOGGING = {
-    # ... (your existing logging config)
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {"level": "INFO", "handlers": ["file"]},
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename":  os.path.join(BASE_DIR, 'social_log.log'),
+            "formatter": "app",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True
+        },
+    },
+    "formatters": {
+        "app": {
+            "format": (
+                u"%(asctime)s [%(levelname)-8s] "
+                "(%(module)s.%(funcName)s) %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
 }
