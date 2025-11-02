05e3637afeeba7dfc47f86122c731780e0ea4a4a
unknown <nwosuemmanuel159@gmail.com>
2025-11-02 09:03:41 +0100

    last

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-# from dotenv import load_dotenv
-# load_dotenv()
+
 # Build paths
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -27,9 +26,6 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'welding-backend-vm1n.onrender.com',
 ]
-
-# print(os.environ.get('SENDGRID_API_KEY'))
-
 
 # Application definition
 INSTALLED_APPS = [
@@ -173,9 +169,9 @@ AUTHENTICATION_BACKENDS = [
 
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY', '')
-DEFAULT_FROM_EMAIL = 'nwosuemmanuel159@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+
